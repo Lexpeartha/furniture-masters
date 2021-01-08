b71b6f642bc43b8198d166e5a4bc4584fb017c89
Lex <sevic.official@gmail.com>
2021-01-08 21:02:36 +0100

    Actually fixing gh-pages error

--- nuxt.config.js
+++ nuxt.config.js
@@ -53,6 +53,7 @@ export default {
 
   // Build Configuration (https://go.nuxtjs.dev/config-build)
   build: {
+    publicPath: '/furniture-masters/_nuxt/',
     extend(config, { isDev, isClient }) {
       if (isDev && isClient) {
         config.module.rules.push({
